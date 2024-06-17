8ba4e7e1a434efd26144c91ddd6a17eb53b9027d
RKulur <rashwithkulur07@gmail.com>
2024-06-17 14:23:06 +0530

    updated authentication

--- backend/src/controllers/authenticationController.ts
+++ backend/src/controllers/authenticationController.ts
@@ -15,7 +15,12 @@ function sendToken(res: Response, payload: authenticationType) {
   const expires = new Date();
   expires.setDate(expires.getDate() + 1);
 
-  res.cookie("token", token, { expires, httpOnly: true, sameSite: "none" });
+  res.cookie("token", token, {
+    expires,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
   return token;
 }
 
