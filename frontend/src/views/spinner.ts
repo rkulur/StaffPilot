1e871e794fdf99ce54dcbc860eb9ab6117beb26d
RKulur <rashwithkulur07@gmail.com>
2024-06-24 21:07:58 +0530

    Minor refactoring

--- frontend/src/views/spinner.ts
+++ frontend/src/views/spinner.ts
@@ -1,7 +1,7 @@
 export const spinner = `
 <div class="w-full h-screen flex flex-col items-center justify-center">
-<div class="scale-150 flex flex-col align-center justify-center rotating-div">
-  <i class="fa-solid fa-spinner"></i>
+<div class="flex flex-col align-center justify-center rotating-div">
+  <i class="fa-solid fa-spinner scale-150 "></i>
 </div>
 </div>
 `;
