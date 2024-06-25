e51a80f7ecf872ba5104d75057731f7917aa8a0e
RKulur <rashwithkulur07@gmail.com>
2024-06-25 20:32:13 +0530

    Minor refactoring

--- frontend/src/views/spinner.ts
+++ frontend/src/views/spinner.ts
@@ -3,5 +3,6 @@ export const spinner = `
 <div class="h-12 w-12 flex flex-col items-center justify-center rotate-spinner scale-200">
   <i class="fa-solid text-yellow-400 font-bold fa-circle-notch origin-center"></i>
 </div>
+<p class="font-bold text-md">Loading...</p>
 </div>
 `;
